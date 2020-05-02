e2595e9e1d8a7cc4cdc1e7a693a3aad08f3474d5
raphaelmazor <raphaelm@sccwrp.org>
2020-05-02 12:41:49 -0700

    Added all key supplemental reports to function

--- swampify_csci.R
+++ swampify_csci.R
@@ -1,34 +1,38 @@
 library(tidyverse)
-#Only covers core results for now
-#Many fields require manual filling. These are left as blank ("") for manual entry in Excel.
 
+#Many fields require manual filling. These are left as blank ("") for manual entry in Excel.
 swampify_CSCI<-function(x){
-  core<-x$core %>%
-    select(StationCode,
-           SampleID,
-           Count,
-           Number_of_MMI_Iterations,
-           Number_of_OE_Iterations, Pcnt_Ambiguous_Individuals,
-           Pcnt_Ambiguous_Taxa,
-           E,
-           Mean_O,
-           OoverE,
-           CSCI,
-           CSCI_Percentile) %>%
-    pivot_longer(cols = c(Count,
-                          Number_of_MMI_Iterations,
-                          Number_of_OE_Iterations, Pcnt_Ambiguous_Individuals,
-                          Pcnt_Ambiguous_Taxa,
-                          E,
-                          Mean_O,
-                          OoverE,
-                          CSCI,
-                          CSCI_Percentile),
-                 names_to = "AnalyteName",
-                 values_to = "Result") %>%
-    mutate(AnalyteName = case_when(AnalyteName=="CSCI_Percentile"~"CSCI_Percentile",
-                                   T~paste0("CSCI_", AnalyteName)),
-           SampleDate="",
+  #Core, Suppl1_mmi, and Suppl1_grps
+  all_but_oe<-x$core %>%
+    left_join(x$Suppl1_mmi) %>%
+    left_join(x$Suppl1_grps) %>%
+    select(-MMI_Percentile, -OoverE_Percentile, -MMI_Score) %>%
+    # names() %>%dput()
+    pivot_longer(cols = c(
+      #From core report
+      Count,Number_of_MMI_Iterations,Number_of_OE_Iterations, Pcnt_Ambiguous_Individuals,Pcnt_Ambiguous_Taxa,
+      E,Mean_O,OoverE,CSCI,CSCI_Percentile,
+      #From Suppl1_mmi
+      Clinger_PercentTaxa, Clinger_PercentTaxa_predicted, Clinger_PercentTaxa_score, 
+      Coleoptera_PercentTaxa, Coleoptera_PercentTaxa_predicted, Coleoptera_PercentTaxa_score, 
+      Taxonomic_Richness, Taxonomic_Richness_predicted, Taxonomic_Richness_score, 
+      EPT_PercentTaxa, EPT_PercentTaxa_predicted, EPT_PercentTaxa_score, 
+      Shredder_Taxa, Shredder_Taxa_predicted, Shredder_Taxa_score, 
+      Intolerant_Percent, Intolerant_Percent_predicted, Intolerant_Percent_score,
+      #From Suppl1_grps
+      pGroup1, pGroup2, pGroup3, pGroup4, pGroup5, pGroup6, pGroup7, pGroup8, pGroup9, pGroup10, pGroup11
+    ),
+    names_to = "AnalyteName",
+    values_to = "Result") %>%
+    mutate(AnalyteName = case_when(AnalyteName=="CSCI_Percentile"~"CSCI_Percentile", #The only variable that doesn't follow this naming convention
+                                   T~paste0("CSCI_", AnalyteName)))
+  #Do Suppl1_OE separately because it's already in long format and because it requires different analyte renaming.
+  just_oe<-x$Suppl1_OE %>%
+    select(StationCode, SampleID, AnalyteName=OTU, Result=CaptureProb) %>%
+    mutate(AnalyteName=paste0("CSCI_Pc_",AnalyteName))
+  #Bind, add other SWAMP fields, and re-order
+  xdf<-bind_rows(all_but_oe, just_oe) %>%
+    mutate(SampleDate="",
            ProjectCode="",
            EventCode="BA",
            ProtocolCode="", #Or always "SWAMP_2016_WS"?
@@ -60,8 +64,7 @@ swampify_CSCI<-function(x){
            FractionName, UnitName, VariableResult, Result, ResQualCode, 
            QACode, ComplianceCode, BatchVerificationCode, CollectionDeviceName, 
            HabitatResultComments)
-  bind_rows(core)
-  
+  xdf
 }
 
 ###EXAMPLE
@@ -69,5 +72,4 @@ swampify_CSCI<-function(x){
 library(CSCI)
 example(CSCI)
 
-results_swampified<-swampify_CSCI(results)
-
+results_swampified<-  swampify_CSCI(results) 
